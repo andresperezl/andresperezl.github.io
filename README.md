a0a44d9854f21e66c8da368d2a274b5dabf5581b
Andres Perez <1676612+andresperezl@users.noreply.github.com>
2026-05-29 22:43:33 -0400

    Update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # andresperezl.github.io
-Hey there! You can checkout this site here: http://andresperezl.com
+Hey there! You can check out this site here: https://andresperezl.com
--- snake/bit.js
+++ snake/bit.js
@@ -19,6 +19,11 @@ pause = true
 fruit = new Array(2)
 fruitColor = "yellow"
 speed = 10
+HUD_HEIGHT = 34
+SNAKE_GREEN = "#22c55e"
+SNAKE_HEAD = "#86efac"
+SNAKE_EAT = "#facc15"
+SNAKE_DEAD = "#fb7185"
 
 prevX=0
 prevY=0
@@ -87,36 +92,160 @@ function play(){
 }
 
 function draw(){
-    ctx.fillStyle = "black"
-    ctx.fillRect(0,0, canvas.width, canvas.height)
-    ctx.fillStyle = "white"
-    ctx.font = "10px sans-serif"
-    ctx.fillText("Score: " + snake.getLength(), 10, 12)
-    ctx.fillText(MAP_SIZE_TEXT, canvas.width - MAP_SIZE_TEXT_WIDTH - 10, 12)
-    speed_text = "Speed: " + speed + " - Press \"+\" or \"-\" to increase or descrease, SPACE to Pause"
-    ctx.fillText(speed_text, canvas.width/2 - ctx.measureText(speed_text).width/2, 12)
+    drawBackground()
+    drawHud()
     drawSnake()
     drawFruit()
 }
 
-function drawSnake() {
+function roundedRect(x, y, width, height, radius) {
+    radius = Math.min(radius, width / 2, height / 2)
+    ctx.beginPath()
+    ctx.moveTo(x + radius, y)
+    ctx.lineTo(x + width - radius, y)
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius)
+    ctx.lineTo(x + width, y + height - radius)
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height)
+    ctx.lineTo(x + radius, y + height)
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius)
+    ctx.lineTo(x, y + radius)
+    ctx.quadraticCurveTo(x, y, x + radius, y)
+    ctx.closePath()
+}
+
+function drawBackground() {
+    background = ctx.createLinearGradient(0, 0, canvas.width, canvas.height)
+    background.addColorStop(0, "#061512")
+    background.addColorStop(0.48, "#0b1f1d")
+    background.addColorStop(1, "#172617")
+    ctx.fillStyle = background
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    ctx.save()
+    ctx.strokeStyle = "rgba(240, 253, 244, 0.055)"
     ctx.lineWidth = 1
-    ctx.fillStyle = snakeState
-    ctx.strokeStyle = "black"
+    for(x = 0.5; x < canvas.width; x += SQUARE_SIZE) {
+        ctx.beginPath()
+        ctx.moveTo(x, HUD_HEIGHT)
+        ctx.lineTo(x, canvas.height)
+        ctx.stroke()
+    }
+    for(y = HUD_HEIGHT + 0.5; y < canvas.height; y += SQUARE_SIZE) {
+        ctx.beginPath()
+        ctx.moveTo(0, y)
+        ctx.lineTo(canvas.width, y)
+        ctx.stroke()
+    }
+    ctx.restore()
+}
+
+function drawHud() {
+    ctx.save()
+    hud = ctx.createLinearGradient(0, 0, 0, HUD_HEIGHT)
+    hud.addColorStop(0, "rgba(2, 6, 23, 0.88)")
+    hud.addColorStop(1, "rgba(2, 6, 23, 0.45)")
+    ctx.fillStyle = hud
+    ctx.fillRect(0, 0, canvas.width, HUD_HEIGHT)
+
+    ctx.font = "700 12px system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif"
+    ctx.textBaseline = "middle"
+    ctx.fillStyle = "#ecfdf5"
+
+    scoreText = "Score " + snake.getLength()
+    roundedRect(8, 7, ctx.measureText(scoreText).width + 22, 20, 999)
+    ctx.fillStyle = "rgba(34, 197, 94, 0.18)"
+    ctx.fill()
+    ctx.fillStyle = "#ecfdf5"
+    ctx.fillText(scoreText, 19, 17)
+
+    mapTextWidth = ctx.measureText(MAP_SIZE_TEXT).width
+    ctx.fillStyle = "rgba(236, 253, 245, 0.72)"
+    ctx.fillText(MAP_SIZE_TEXT, canvas.width - mapTextWidth - 10, 17)
+
+    speed_text = canvas.width < 720 ? "Speed " + speed + " | SPACE pause" : "Speed " + speed + " | +/- speed | SPACE pause | Arrows or WASD"
+    speedWidth = ctx.measureText(speed_text).width
+    ctx.fillText(speed_text, canvas.width/2 - speedWidth/2, 17)
+    ctx.restore()
+}
+
+function drawSnake() {
+    ctx.save()
+    ctx.lineWidth = 1.5
+    ctx.shadowColor = "rgba(34, 197, 94, 0.3)"
+    ctx.shadowBlur = snakeState == 'red' ? 0 : 8
     for(i = 0; i < snake.getLength(); i++) {
         element = snake.get(i)
-        ctx.fillRect(element[0] * SQUARE_SIZE,  element[1] * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
-        ctx.strokeRect(element[0] * SQUARE_SIZE,  element[1] * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+        isHead = i == snake.getLength() - 1
+        padding = isHead ? 1 : 2
+        x = element[0] * SQUARE_SIZE + padding
+        y = element[1] * SQUARE_SIZE + padding
+        size = SQUARE_SIZE - padding * 2
+        ctx.fillStyle = snakeFill(i, isHead)
+        ctx.strokeStyle = snakeState == 'red' ? "rgba(127, 29, 29, 0.8)" : "rgba(187, 247, 208, 0.45)"
+        roundedRect(x, y, size, size, isHead ? 6 : 5)
+        ctx.fill()
+        ctx.stroke()
+        if(isHead) {
+            drawSnakeEyes(x, y, size)
+        }
     }
+    ctx.restore()
+}
+
+function snakeFill(index, isHead) {
+    if(snakeState == 'red') {
+        return SNAKE_DEAD
+    }
+    if(snakeState == 'yellow') {
+        return isHead ? "#fef08a" : SNAKE_EAT
+    }
+    lightness = 42 + Math.min(18, index)
+    return isHead ? SNAKE_HEAD : "hsl(142, 72%, " + lightness + "%)"
+}
+
+function drawSnakeEyes(x, y, size) {
+    eyeOffsetX = size * 0.28
+    eyeOffsetY = size * 0.3
+    if(direction == 'L' || direction == 'R') {
+        eyeX = direction == 'R' ? x + size * 0.68 : x + size * 0.32
+        eyes = [[eyeX, y + eyeOffsetY], [eyeX, y + size - eyeOffsetY]]
+    } else {
+        eyeY = direction == 'D' ? y + size * 0.68 : y + size * 0.32
+        eyes = [[x + eyeOffsetX, eyeY], [x + size - eyeOffsetX, eyeY]]
+    }
+
+    ctx.save()
+    ctx.shadowBlur = 0
+    ctx.fillStyle = snakeState == 'red' ? "#450a0a" : "#052e16"
+    for(j = 0; j < eyes.length; j++) {
+        ctx.beginPath()
+        ctx.arc(eyes[j][0], eyes[j][1], 1.7, 0, 2 * Math.PI)
+        ctx.fill()
+    }
+    ctx.restore()
 }
 
 function drawFruit() {
     x = fruit[0] * SQUARE_SIZE + R
     y = fruit[1] * SQUARE_SIZE + R
-    ctx.fillStyle = fruitColor
+    ctx.save()
+    ctx.shadowColor = fruitColor
+    ctx.shadowBlur = 16
+    fruitGradient = ctx.createRadialGradient(x - 3, y - 4, 1, x, y, R)
+    fruitGradient.addColorStop(0, "#ffffff")
+    fruitGradient.addColorStop(0.18, fruitColor)
+    fruitGradient.addColorStop(1, "rgba(251, 113, 133, 0.95)")
+    ctx.fillStyle = fruitGradient
     ctx.beginPath()
-    ctx.arc(x, y, R, 0, 2 * Math.PI)
+    ctx.arc(x, y, R - 1, 0, 2 * Math.PI)
     ctx.fill()
+
+    ctx.shadowBlur = 0
+    ctx.fillStyle = "rgba(255, 255, 255, 0.85)"
+    ctx.beginPath()
+    ctx.arc(x - 3, y - 4, 2, 0, 2 * Math.PI)
+    ctx.fill()
+    ctx.restore()
 }
 
 document.onkeydown = function(evt) {
@@ -200,9 +329,31 @@ canvas.ontouchmove = function(evt) {
 }
 
 draw()
-ctx.fillStyle = 'white'
-ctx.font = "48px san-serif"
-start_text = "Press SPACE or TOUCH to start playing"
-start_text_width = ctx.measureText(start_text).width
-ctx.fillText(start_text, canvas.width/2 - start_text_width/2, canvas.height/2 - 24)
+drawStartMessage()
 gameLoop = setInterval(play, 1000/speed)
+
+function drawStartMessage() {
+    panelWidth = Math.min(canvas.width - 32, 520)
+    panelHeight = 132
+    panelX = canvas.width/2 - panelWidth/2
+    panelY = canvas.height/2 - panelHeight/2
+
+    ctx.save()
+    ctx.fillStyle = "#020617"
+    roundedRect(panelX, panelY, panelWidth, panelHeight, 28)
+    ctx.fill()
+    ctx.strokeStyle = "rgba(187, 247, 208, 0.24)"
+    ctx.stroke()
+
+    ctx.textAlign = "center"
+    ctx.fillStyle = "#ecfdf5"
+    ctx.font = "700 42px Georgia, 'Times New Roman', serif"
+    ctx.fillText("Snake", canvas.width/2, panelY + 52)
+    ctx.font = "700 16px system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif"
+    ctx.fillStyle = "#bbf7d0"
+    ctx.fillText("Press SPACE or touch to start", canvas.width/2, panelY + 84)
+    ctx.font = "500 12px system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif"
+    ctx.fillStyle = "rgba(236, 253, 245, 0.7)"
+    ctx.fillText("Arrow keys / WASD to move", canvas.width/2, panelY + 108)
+    ctx.restore()
+}
